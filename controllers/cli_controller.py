7dfb67fda2c4f3b6d1532309ffb323089497a536
Tilley <tookie@Tilley-MBP.localdomain>
2024-11-21 20:18:01 +1100

    create drop and seed transactions, portfolios and watchlists tables, correction to trasaction.py for nullable=True

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -3,9 +3,8 @@ from datetime import date
 from flask import Blueprint
 
 from init import db
-from models import User, Stock, Order
+from models import User, Stock, Order, Portfolio, Transaction, Watchlist
 from enums import OrderType, OrderStatus, TransactionType
-
 
 
 db_commands = Blueprint("db", __name__)
@@ -22,89 +21,89 @@ def drop_tables():
 
 @db_commands.cli.command("seed")
 def seed_tables():
-    users = [
+     users = [
           User(f_name="Papa",
                l_name="Li",
                email="papali@email.com",
                account_open_date=date(2020,3,14),
                account_balance=4521546.22
-               ),
+          ),
           User(f_name="Mama",
                l_name="Ti",
                email="mamati@email.com",
                account_open_date=date(2021,11,8),
                account_balance=256349.12
-               ),
+          ),
           User(f_name="Yogi",
                l_name="Bear",
                email="yogibear@email.com",
                account_open_date=date(2022,8, 7),
                account_balance=803241.69
-               ),
+          ),
           User(f_name="Pepe",
                l_name="Poo",
                email="pepepoo@email.com",
                account_open_date=date(2022,11,7),
                account_balance=4521546.22
-               ),
+          ),
           User(f_name="Bandit",
                l_name="Boy",
                email="banditboy@email.com",
                account_open_date=date(2024,5,8),
                account_balance=4512.58
                )
-    ]
+     ]
 
-    db.session.add_all(users)
+     db.session.add_all(users)
 
-    stocks = [
+     stocks = [
           Stock(stock_name="Commonwealth Bank",
                ticker="CBA",
                stock_price=142.93
-               ),
+          ),
           Stock(stock_name="CSL Limited",
                ticker="CSL",
                stock_price=287.46
-               ),
+          ),
           Stock(stock_name="ANZ Group Holdings Limited",
                ticker="ANZ",
                stock_price=31.22
-               ),
+          ),
           Stock(stock_name="Westpac Banking Group",
                ticker="WBC",
                stock_price=32.01
-               ),
+          ),
           Stock(stock_name="Rio Tinto",
                ticker="RIO",
                stock_price=120.31
-               ),
+          ),
           Stock(stock_name="Macquarie Group Limited",
                ticker="MQG",
                stock_price=231.37
-               ),
+          ),
           Stock(stock_name="Telstra Group Limited",
                ticker="TLS",
                stock_price=3.83
-               ),
+          ),
           Stock(stock_name="Xero",
                ticker="XRO",
                stock_price=150.00
-               ),
+          ),
           Stock(stock_name="ResMed Inc",
                ticker="RMD",
                stock_price=37.32
-               ),
+          ),
           Stock(stock_name="Coles Group Limited",
                ticker="COL",
                stock_price=17.70
-               )
+          )
      ]
 
-    db.session.add_all(stocks)
+     db.session.add_all(stocks)
 
-    db.session.commit()
+     db.session.commit()
 
-    orders = [
+     orders = [
           Order(trade_date=date(2024,10,30),
                order_type=OrderType.BUY,
                quantity=50,
@@ -112,7 +111,7 @@ def seed_tables():
                order_status=OrderStatus.PENDING,
                user_id=5,
                stock_id=1,
-               ),
+          ),
           Order(trade_date=date(2024,6,4),
                order_type=OrderType.BUY,
                quantity=42,
@@ -120,7 +119,7 @@ def seed_tables():
                order_status=OrderStatus.EXECUTED,
                user_id=2,
                stock_id=5,
-               ),
+          ),
           Order(trade_date=date(2024,1,6),
                order_type=OrderType.BUY,
                quantity=20,
@@ -128,7 +127,7 @@ def seed_tables():
                order_status=OrderStatus.EXECUTED,
                user_id=3,
                stock_id=10,
-               ),
+          ),
           Order(trade_date=date(2023,12,22),
                order_type=OrderType.BUY,
                quantity=10,
@@ -136,7 +135,7 @@ def seed_tables():
                order_status=OrderStatus.EXECUTED,
                user_id=4,
                stock_id=9,
-               ),
+          ),
           Order(trade_date=date(2023,12,12),
                order_type=OrderType.BUY,
                quantity=12,
@@ -144,7 +143,7 @@ def seed_tables():
                order_status=OrderStatus.EXECUTED,
                user_id=1,
                stock_id=6,
-               ),
+          ),
           Order(trade_date=date(2023,12,6),
                order_type=OrderType.BUY,
                quantity=15,
@@ -152,7 +151,7 @@ def seed_tables():
                order_status=OrderStatus.EXECUTED,
                user_id=4,
                stock_id=3,
-               ),
+          ),
           Order(trade_date=date(2023,11,4),
                order_type=OrderType.BUY,
                quantity=20,
@@ -160,7 +159,7 @@ def seed_tables():
                order_status=OrderStatus.EXECUTED,
                user_id=3,
                stock_id=8,
-               ),
+          ),
           Order(trade_date=date(2023,10,18),
                order_type=OrderType.BUY,
                quantity=63,
@@ -168,7 +167,7 @@ def seed_tables():
                order_status=OrderStatus.EXECUTED,
                user_id=2,
                stock_id=7,
-               ),
+          ),
           Order(trade_date=date(2023,9,23),
                order_type=OrderType.BUY,
                quantity=8,
@@ -176,7 +175,7 @@ def seed_tables():
                order_status=OrderStatus.EXECUTED,
                user_id=2,
                stock_id=6,
-               ),
+          ),
           Order(trade_date=date(2023,9,19),
                order_type=OrderType.BUY,
                quantity=72,
@@ -184,12 +183,221 @@ def seed_tables():
                order_status=OrderStatus.EXECUTED,
                user_id=1,
                stock_id=3
-               )
-    ]
+          )
+     ]
 
-    db.session.add_all(orders)
+     db.session.add_all(orders)
 
-    db.session.commit()
+     portfolios = [
+          Portfolio(number_of_units=20,
+                    user_id=2,
+                    stock_id=2,
+          ),
+          Portfolio(number_of_units=15,
+                    user_id=4,
+                    stock_id=3,
+          ),
+          Portfolio(number_of_units=42,
+                    user_id=5,
+                    stock_id=4,
+          ),
+          Portfolio(number_of_units=35,
+                    user_id=2,
+                    stock_id=5,
+          ),
+          Portfolio(number_of_units=20,
+                    user_id=3,
+                    stock_id=10,
+          ),
+          Portfolio(number_of_units=12,
+                    user_id=1,
+                    stock_id=6,
+          ),
+          Portfolio(number_of_units=20,
+                    user_id=3,
+                    stock_id=8,
+          ),
+          Portfolio(number_of_units=10,
+                    user_id=4,
+                    stock_id=9,
+          ),
+          Portfolio(number_of_units=63,
+                    user_id=2,
+                    stock_id=7,
+          ),
+          Portfolio(number_of_units=8,
+                    user_id=2,
+                    stock_id=6,
+          ),
+          Portfolio(number_of_units=72,
+                    user_id=1,
+                    stock_id=3,
+          )   
+     ]
+
+     db.session.add_all(portfolios)
+
+     transactions = [
+          Transaction(transaction_date=date(2024,10,30),
+                      transaction_type=TransactionType.BUY,
+                      amount=7146.50,
+                      user_id=5,
+                      order_id=1,              
+          ),
+          Transaction(transaction_date=date(2024,10,30),
+                      transaction_type=TransactionType.DEPOSIT,
+                      amount=7146.50,
+                      user_id=5,
+                      order_id=None,              
+          ),
+          Transaction(transaction_date=date(2024,6,14),
+                      transaction_type=TransactionType.BUY,
+                      amount=1344.42,
+                      user_id=5,
+                      order_id=4,              
+          ),
+          Transaction(transaction_date=date(2024,5,12),
+                      transaction_type=TransactionType.DEPOSIT,
+                      amount=5857,
+                      user_id=5,
+                      order_id=None,              
+          ),
+          Transaction(transaction_date=date(2024,3,12),
+                      transaction_type=TransactionType.BUY,
+                      amount=5749.20,
+                      user_id=2,
+                      order_id=2,              
+          ),
+          Transaction(transaction_date=date(2024,3,20),
+                      transaction_type=TransactionType.SELL,
+                      amount=468.30,
+                      user_id=4,
+                      order_id=3,              
+          ),
+          Transaction(transaction_date=date(2024,2,3),
+                      transaction_type=TransactionType.BUY,
+                      amount=4210.85,
+                      user_id=2,
+                      order_id=5,              
+          ),
+          Transaction(transaction_date=date(2024,1,6),
+                      transaction_type=TransactionType.BUY,
+                      amount=354.00,
+                      user_id=3,
+                      order_id=10,              
+          ),
+          Transaction(transaction_date=date(2024,1,4),
+                      transaction_type=TransactionType.WITHDRAWAL,
+                      amount=5000,
+                      user_id=1,
+                      order_id=None,              
+          ),
+          Transaction(transaction_date=date(2023,12,22),
+                      transaction_type=TransactionType.BUY,
+                      amount=373.20,
+                      user_id=4,
+                      order_id=9,              
+          ),
+          Transaction(transaction_date=date(2023,12,12),
+                      transaction_type=TransactionType.BUY,
+                      amount=2776.44,
+                      user_id=1,
+                      order_id=6,              
+          ),
+          Transaction(transaction_date=date(2023,12,6),
+                      transaction_type=TransactionType.BUY,
+                      amount=468.30,
+                      user_id=4,
+                      order_id=3,              
+          ),
+          Transaction(transaction_date=date(2023,11,29),
+                      transaction_type=TransactionType.DEPOSIT,
+                      amount=6073.28,
+                      user_id=4,
+                      order_id=None,              
+          ),
+          Transaction(transaction_date=date(2023,11,4),
+                      transaction_type=TransactionType.BUY,
+                      amount=3000.00,
+                      user_id=3,
+                      order_id=8,              
+          ),
+          Transaction(transaction_date=date(2023,11,2),
+                      transaction_type=TransactionType.DEPOSIT,
+                      amount=806595.69,
+                      user_id=3,
+                      order_id=None,              
+          ),
+          Transaction(transaction_date=date(2023,10,26),
+                      transaction_type=TransactionType.WITHDRAWAL,
+                      amount=2000,
+                      user_id=2,
+                      order_id=None,              
+          ),
+          Transaction(transaction_date=date(2023,10,18),
+                      transaction_type=TransactionType.BUY,
+                      amount=241.29,
+                      user_id=2,
+                      order_id=7,              
+          ),
+          Transaction(transaction_date=date(2023,9,23),
+                      transaction_type=TransactionType.BUY,
+                      amount=1850.96,
+                      user_id=2,
+                      order_id=6,              
+          ),
+          Transaction(transaction_date=date(2023,9,19),
+                      transaction_type=TransactionType.BUY,
+                      amount=2247.84,
+                      user_id=1,
+                      order_id=3,              
+          ),
+          Transaction(transaction_date=date(2023,8,8),
+                      transaction_type=TransactionType.DEPOSIT,
+                      amount=270401.42,
+                      user_id=2,
+                      order_id=None,              
+          ),
+          Transaction(transaction_date=date(2023,7,17),
+                      transaction_type=TransactionType.DEPOSIT,
+                      amount=4531570.50,
+                      user_id=1,
+                      order_id=None,              
+          ),
+     ]
+
+     db.session.add_all(transactions)
+
+     watchlists = [
+          Watchlist(user_id=4,
+                    stock_id=5,
+          ),
+          Watchlist(user_id=1,
+                    stock_id=7,
+               ),
+          Watchlist(user_id=3,
+                    stock_id=6,
+               ),
+          Watchlist(user_id=5,
+                    stock_id=8,
+               ),
+          Watchlist(user_id=2,
+                    stock_id=10,
+               ),
+          Watchlist(user_id=3,
+                    stock_id=4,
+               ),
+          Watchlist(user_id=2,
+                    stock_id=10,
+               ),
+          Watchlist(user_id=1,
+                    stock_id=4,
+               ),   
+     ]
+
+     db.session.add_all(watchlists)
+
+     db.session.commit()
 
 
-    print("Tables seeded")
+     print("Tables seeded")
--- models/transaction.py
+++ models/transaction.py
@@ -15,7 +15,7 @@ class Transaction(db.Model):
     transaction_type = db.Column(db.Enum(TransactionType), nullable=False) # this has enumerate
     amount = db.Column(db.Numeric(precision=15, scale=2),nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"), nullable=True)
     # relationships can have many transactions but to 1 user and order
     user = db.relationship("User", back_populates="transactions")
     order = db.relationship("Order", back_populates="transaction")
