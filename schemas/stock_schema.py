f3a54c4778185eecf2fd52cb7372158bd1e6085c
Tilley <tookie@Tilley-MBP.localdomain>
2024-11-26 23:39:37 +1100

    added fields.Nested / List to order, user and stock schema

--- schemas/stock_schema.py
+++ schemas/stock_schema.py
@@ -1,9 +1,12 @@
+from marshmallow import fields
+
 from init import ma
 
 class StockSchema(ma.Schema):
     ordered=True
+    orders = fields.List(fields.Nested("OrderSchema", exclude=["stock", "user"]))
     class Meta:
-        fields = ("id", "stock_name", "ticker", "stock_price")
+        fields = ("id", "stock_name", "ticker", "stock_price", "orders")
 
 stock_schema = StockSchema()
 stocks_schema = StockSchema(many=True)
--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,9 +1,12 @@
+from marshmallow import fields
+
 from init import ma
 
 class UserSchema(ma.Schema):
     ordered=True
+    orders = fields.List(fields.Nested("OrderSchema", only=["trade_date", "order_type", "net_amount", "stock_id"], exclude=["user", "stock"]))
     class Meta:
-        fields = ("id", "f_name", "l_name", "email", "account_open_date", "account_balance")
+        fields = ("id", "f_name", "l_name", "email", "account_open_date", "account_balance", "orders")
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
