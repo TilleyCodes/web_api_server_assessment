e917e1a149cec37b767a323073495eb2c111a20d
Tilley <tookie@Tilley-MBP.localdomain>
2024-12-18 20:16:59 +1100

    removing some constraints added to models to eliminate the errors

--- models/portfolio.py
+++ models/portfolio.py
@@ -8,7 +8,7 @@ class Portfolio(db.Model):
     __tablename__ = "portfolios"
 
     id = db.Column(db.Integer, primary_key=True)
-    number_of_units = db.Column(db.Integer, nullable=False, check=db.CheckConstraint("number_of_units >= 0"))
+    number_of_units = db.Column(db.Integer, nullable=False)
     investor_id = db.Column(db.Integer, db.ForeignKey("investors.id"), nullable=False)
     stock_id = db.Column(db.Integer, db.ForeignKey("stocks.id"), nullable=False)
     # relationships can have many portfolios but to 1 investor and 1 stock
--- models/stock.py
+++ models/stock.py
@@ -10,7 +10,7 @@ class Stock(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     stock_name = db.Column(db.String(100), nullable=False, unique=True)
     ticker = db.Column(db.String(100), nullable=False, unique=True)
-    stock_price = db.Column(db.Numeric(precision=15, scale=2),nullable=False, check=db.CheckConstraint("stock_price >= 0"))
+    stock_price = db.Column(db.Numeric(precision=15, scale=2),nullable=False)
     # Relationship 1 stock can have many. Need to have cascade as it is referenced to other tables
     orders = db.relationship("Order", back_populates = "stock", cascade="all, delete-orphan")
     portfolios = db.relationship("Portfolio", back_populates = "stock", cascade="all, delete-orphan")
